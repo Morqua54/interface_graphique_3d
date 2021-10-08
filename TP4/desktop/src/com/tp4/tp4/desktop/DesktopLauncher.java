dd550144dbaccff1bbd4dafc29fff1683a8636db
Mathilde Nicolas <mathilde.nicolas54170@gmail.com>
2021-10-08 11:28:47 +0200

    End of the game

--- TP4/desktop/src/com/tp4/tp4/desktop/DesktopLauncher.java
+++ TP4/desktop/src/com/tp4/tp4/desktop/DesktopLauncher.java
@@ -1,12 +1,17 @@
 package com.tp4.tp4.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tp4.tp4.Application;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Application(), config);
-	}
+
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        new Lwjgl3Application(new Application(), config);
+        config.setTitle("Drop");
+        config.setWindowedMode(800,480);
+        new Lwjgl3Application(new Application(), config);
+    }
+
 }
